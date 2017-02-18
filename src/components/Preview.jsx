e4fce4ea7fe4daaf3c45ff4cadbc96d00fd1d97d
Bart Wijnants <bartwijnants@outlook.com>
2017-02-18 18:44:16 +0100

    make markdown links open in new tab

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -4,7 +4,15 @@ import showdown from 'showdown';
 export default class Preview extends React.Component {
   constructor(props) {
     super(props);
-    this.converter = new showdown.Converter();
+    showdown.setFlavor('github');
+    showdown.extension('targetlink', function () {
+      return [{
+        type: 'html',
+        regex: /(<a [^>]+?)(>.*<\/a>)/g,
+        replace: '$1 target="_blank"$2'
+      }];
+    });
+    this.converter = new showdown.Converter({ extensions: ['targetlink'] });
   }
   render() {
     const slideHtml = { __html: this.converter.makeHtml(this.props.slide.content) };
