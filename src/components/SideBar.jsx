7135c27a88864912927669d41bc66de5010a4b13
Mihai Panait <mihai.panait@outlook.com>
2017-02-22 23:43:45 +0200

    removed github icon from header
    too crowded

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -10,7 +10,7 @@ export default class SideBar extends React.Component {
         <div className="header">
           <div className="center">        
             <img src={this.props.isDark ? "assets/logo-dark.png" : "assets/logo.png"} alt="slidux" />
-            <p className="tag-line">by <a href="https://bartw.github.io/" target="_blank">Bart Wijnants <i className="fa fa-github" aria-hidden="true"></i></a></p>
+            <p className="tag-line">by <a href="https://bartw.github.io/" target="_blank">Bart Wijnants</a></p>
             <a className="btn btn-link" href="https://github.com/bartw/slidux" target="_blank"><span><i className="fa fa-code-fork" aria-hidden="true"></i> Fork me on GitHub</span></a>
           </div>
         </div>
